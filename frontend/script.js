5dff70c644df746a21ac18dcc2f695d5c9037532
avolve-here <pcworkfor16@gmail.com>
2026-09-24 17:42:46 +0530

    Use original SpotDL backend

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@ const API_BASE =
     window.location.hostname === "127.0.0.1" ||
     window.location.hostname === "localhost"
         ? "http://127.0.0.1:8001"
-        : "";
+        : "https://music-downloader-pupp.onrender.com";
 
 
 // =========================================================
